df6718be178675816f8444c77c7eadad7add25a1
fanyingying <1178223444@qq.com>
2016-05-18 11:22:02 +0800

    less文件夹

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 教师助手
 
 ## 目录结构
-1、account 用户模块
-2、classes 班级模块
-3、message 消息模块
-4、exam  考试模块
-5、task 作业模块
-6、attend 考勤、请假模块 
+1、account 用户模块<br>
+2、classes 班级模块<br>
+3、message 消息模块<br>
+4、exam  考试模块<br>
+5、task 作业模块<br>
+6、attend 考勤、请假模块 <br>
 7、
 
 ## css说明
-1、less文件夹下面为less文件编译后生成同名css文件到css目录下
-2、mui.min.css  mui框架css
-3、style.css    mui重定义的、通用css
-4、account.css  用户相关的css
+1、less文件夹下面为less文件编译后生成同名css文件到css目录下<br>
+2、mui.min.css  mui框架css<br>
+3、style.css    mui重定义的、通用css<br>
+4、account.css  用户相关的css<br>
 
 ## gulp配置 利用gulp进行js、css压缩等工作
---
+++ js/message.js
@@ -0,0 +1,71 @@
+/**
+ * 消息模块
+ **/
+(function($, owner) {
+	/**
+	 * 根据条件获取消息列表
+	 * @param {Object} 参数
+	 * @param {Object} 回调
+	 */
+	owner.getList = function(params, callback) {
+		callback = callback || $.noop;
+		params = params || {};
+		mui.ajax('', {
+			data: params,
+			dataType: 'json', //服务器返回json格式数据
+			type: 'post', //HTTP请求类型
+			timeout: 10000, //超时时间设置为10秒；
+			success: function(data) {
+				 				 
+			},
+			error: function(xhr, type, errorThrown) {
+				//异常处理；
+				console.log(type);
+				return callback('错误提示');
+			}
+		});
+	};
+
+	/**
+	 * 下拉刷新具体业务实现
+	 */
+	owner.pulldownRefresh = function(params) {
+		owner.getList(params, function(err) {
+			mui('#refreshContainer').pullRefresh().endPulldownToRefresh();
+			if (err) {
+				mui.toast(err);
+				return;
+			}
+			
+		});
+		setTimeout(function() {
+			var table = document.body.querySelector('.mui-table-view');
+			var cells = document.body.querySelectorAll('.mui-table-view-cell');
+			for (var i = cells.length, len = i + 3; i < len; i++) {
+				var li = document.createElement('li');
+				li.className = 'mui-table-view-cell';
+				li.innerHTML = '<a class="mui-navigate-right">Item ' + (i + 1) + '</a>';
+				//下拉刷新，新纪录插到最前面；
+				table.insertBefore(li, table.firstChild);
+			}
+			mui('#refreshContainer').pullRefresh().endPulldownToRefresh();
+		}, 1500);
+	};
+	var count = 0;
+	/**
+	 * 上拉加载具体业务实现
+	 */
+	owner.pullupRefresh = function() {
+		setTimeout(function() {
+			mui('#refreshContainer').pullRefresh().endPullupToRefresh((++count > 2)); //参数为true代表没有更多数据了。
+			var table = document.body.querySelector('.mui-table-view');
+			var cells = document.body.querySelectorAll('.mui-table-view-cell');
+			for (var i = cells.length, len = i + 20; i < len; i++) {
+				var li = document.createElement('li');
+				li.className = 'mui-table-view-cell';
+				li.innerHTML = '<a class="mui-navigate-right">Item ' + (i + 1) + '</a>';
+				table.appendChild(li);
+			}
+		}, 1500);
+	};
+})(mui, window.message = {});
